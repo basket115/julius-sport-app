2b027d84fae29bb380d1436b24b853812e8b9730
basket115 <gfxnews.lang@gmail.com>
2026-03-10 08:25:14 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,8 @@ script.src = 'https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js';
 script.defer = true;
 document.head.appendChild(script);
 
-// OneSignal initialisieren
+// OneSignal wird dynamisch in App.tsx initialisiert
 window.OneSignalDeferred = window.OneSignalDeferred || [];
-window.OneSignalDeferred.push(async function (OneSignal: any) {
-  await OneSignal.init({
-    appId: '5ecf64c2-2666-4009-ab7d-78474d673a83',  // ✅ TG Neuss Tigers
-  });
-});
 
 const root = document.getElementById('root');
 if (root) {
